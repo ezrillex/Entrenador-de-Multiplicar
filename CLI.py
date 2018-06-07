d55b9e1f050626458e510e08881e5e3b83df8a58
ezrillex <ezrillex@gmail.com>
2018-06-07 02:06:06 -0600

    Made the CLI functional. Main practice functionality is working. Moved some logic that was on the CLI to the trainer module. Implemented exercise generation and difficulty logic.

--- CLI.py
+++ CLI.py
@@ -8,27 +8,40 @@ trainer.check_first_launch()
 
 lang = trainer.get_language_file()
 user_stats = trainer.load_user_stats()
-
-historic = trainer.Counter('historico')
-corrects = trainer.Counter('aciertos')
-fails = trainer.Counter('fallos')
-streaks = trainer.Counter('streaks')
-
-confidence = trainer.Matrix('confidence', 12, 12)
-tables = trainer.Matrix('tables', 12, 12)
-
-# Check if the multiplication tables that were loaded are not empty, if so generate them.
-# This might allow flexibility to change the size in the future by changing the x, y parameters.
-if tables.data[11][11] == 0:
-    for x in range(12):
-        for y in range(12):
-            tables.data[x][y] = (x+1)*(y+1)
-    tables.update()
+exercise = trainer.Exercise()
 
 # TODO Window name according to language
 
 print(lang[0]['11'], user_stats[0]['name'])
 print(lang[0]['12'])
 
+while True:
+    print("\nMain Menu Options:\n1 - Practice Multiplications!\n2 - See Statistics\n3 - Settings\n4 - About\n5 - Exit")
+    selection = int(input("\nWhat do you want to do?\n"))
 
-print("\nMain Menu Options:\n1 - Practice Multiplications!\n2 - Settings\n3 - About\n4 - Exit")
+    if selection == 1:
+        q = int(input("\nHow many exercises do you want to do?\n"))
+
+        for x in range(q):
+            exercise.get_exercise()
+            print("%s * %s = ?" % (str(exercise.current_exercise[0] + 1), str(exercise.current_exercise[1] + 1)))
+            answer = int(input())
+
+            if exercise.check_answer(answer) is True:
+                print("Correcto!")
+            else:
+                # If this is another elif to check if is False then the historic and other counters will increase twice.
+                print("Incorrecto! La respuesta era: ", str(exercise.answer))
+    elif selection == 3:
+        print("\nSettings:\n1 - Reset statistics\n2 - Edit your name\n3 - Go back to the main menu")
+        sub_selection = int(input("\nWhat do you want to do?\n"))
+        if sub_selection == 1:
+            trainer.reset_user_stats()
+            print("Reset successful! All stats have been set to 0.")
+        elif sub_selection == 2:
+            trainer.edit_name()
+        elif sub_selection == 3:
+            continue
+
+    elif selection == 5:
+        break
--- trainer.py
+++ trainer.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 
 # This module works as logic of the program, but some of the methods are only suited to be used by the CLI.
@@ -95,6 +96,17 @@ def save_user_stats(modified_user_stats):
     file.close()
 
 
+# GLOBAL
+# Reset user statistics to 0. Without deleting the name.
+def reset_user_stats():
+    stats = load_user_stats()
+    stats[0]['historico'] = 0
+    stats[0]['fallos'] = 0
+    stats[0]['aciertos'] = 0
+    stats[0]['streaks'] = 0
+    save_user_stats(stats)
+
+
 # CLI
 # Edits the name of the user
 def edit_name():
@@ -108,6 +120,7 @@ def edit_name():
         print(lang[0]['7'])
         user_stats[0]['name'] = str(input())
         save_user_stats(user_stats)
+        print("Name successfully changed %s!" % (user_stats[0]['name']))
 
 
 # GLOBAL
@@ -146,10 +159,7 @@ class Counter(object):
         save[0][self.key] = self.value
         save_user_stats(save)
 
-    def current_value(self):
-        return self.value
-
-
+# GLOBAL
 # Save a matrix given filename and variable containing the matrix to save.
 class Matrix(object):
     def __init__(self, name, dim_x, dim_y):
@@ -193,12 +203,6 @@ class Matrix(object):
         file.write(data)
         file.close()
 
-    # .set(x,y) sets the value of the matrix at a specified coordinate.
-    def set(self, x_coord, y_coord, value):
-        """Provides a way to change a value and save it immediately to file."""
-        self.data[x_coord][y_coord] = value
-        self.save(self.data)
-
     # .update()
     def update(self):
         """Saves the current self.data, useful if .data has been modified directly"""
@@ -209,13 +213,85 @@ class Matrix(object):
         min_x = 0
         min_y = 0
         min_value = self.data[min_x][min_y]
-        for x in range(0,self.dim_x):
-            for y in range(0,self.dim_y):
+        for x in range(0, self.dim_x):
+            for y in range(0, self.dim_y):
                 if self.data[x][y] < min_value:
                     min_x = x
                     min_y = y
                     min_value = self.data[x][y]
+        # Returning the minimum value is unnecessary since we only need to know the x,y of the lowest not how low it is.
         return [min_x, min_y, min_value]
+
+
+# GLOBAL
+# A class with methods such as .get_random and .get_low_confidence, that generate random or low confidence exercises.
+class Exercise(object):
+    def __init__(self):
+        self.historic = Counter('historico')
+        self.corrects = Counter('aciertos')
+        self.fails = Counter('fallos')
+        self.streaks = Counter('streaks')
+        self.confidence = Matrix('confidence', 12, 12)
+        self.tables = Matrix('tables', 12, 12)
+        # Check if the multiplication tables that were loaded are not empty, if so generate them.
+        # This might allow flexibility to change the size in the future by changing the x, y parameters.
+        if self.tables.data[11][11] == 0:
+            for x in range(12):
+                for y in range(12):
+                    self.tables.data[x][y] = (x + 1) * (y + 1)
+            self.tables.update()
+        self.answer = None
+        self.current_exercise = None
+
+    def get_exercise(self):
+        diff = self.get_difficulty()
+        if diff == 1:
+            self.low_confidence_exercise()
+        elif diff == 2:
+            self.random_exercise()
+        else:
+            pass
+
+    # random
+    def random_exercise(self):
+        random_x = random.randint(0, 11)
+        random_y = random.randint(0, 11)
+        self.answer = self.tables.data[random_x][random_y]
+        self.current_exercise = [random_x, random_y]
+        return self.current_exercise
+
+    # low confidence
+    def low_confidence_exercise(self):
+        location = self.confidence.get_minimum_argument()
+        self.answer = self.tables.data[location[0]][location[1]]
+        self.current_exercise = [location[0], location[1]]
+        return self.current_exercise
+
+    def check_answer(self, answer):
+        self.historic.increase()
+        if answer == self.answer:
+            self.corrects.increase()
+            self.streaks.increase()
+            self.confidence.data[self.current_exercise[0]][self.current_exercise[1]] += 1
+            self.confidence.update()
+            return True
+        else:
+            self.streaks.reset()
+            if self.confidence.data[self.current_exercise[0]][self.current_exercise[1]] > 0:
+                self.confidence.data[self.current_exercise[0]][self.current_exercise[1]] -= 1
+                self.confidence.update()
+            self.fails.increase()
+            return False
+
+    def get_difficulty(self):
+        """Determines the current difficulty by checking historic and streak values. Returns 1 or 2 to tell the
+            caller which type of exercise to generate being 1 for low confidence and 2 for random exercise"""
+        rng = random.randint(1, 100)
+        streak_percent = (self.streaks.value // 5)
+        if rng < streak_percent and self.historic.value > 144:
+            return 1
+        else:
+            return 2
 
 
 if __name__ == "__main__":
