1398fed2c8ef9e3ea18710a9a939ffee07d9709f
ezrillex <ezrillex@gmail.com>
2018-06-07 18:58:51 -0600

    made launcher to launch CLI and later the GUI by default.

--- CLI.py
+++ CLI.py
@@ -1,58 +1,60 @@
-# Command Line Interface Version
-import pprint
-import trainer
-import winsound
+def cli():
+    # Command Line Interface Version
+    import pprint
+    import trainer
+    import winsound
 
-# TODO check for crashes if user inputs non integer values and build more robust inputs.
-interface_mode = "CLI"
-trainer.check_first_launch()
-lang = trainer.get_language_file()
-user_stats = trainer.load_user_stats()
-exercise = trainer.Exercise()
+    # TODO check for crashes if user inputs non integer values and build more robust inputs.
+    interface_mode = "CLI"
+    trainer.check_first_launch()
+    lang = trainer.get_language_file()
+    user_stats = trainer.load_user_stats()
+    exercise = trainer.Exercise()
 
-# TODO Window name according to language
-# TODO Implement multilingual support.
-print("Hello!", user_stats[0]['name'])
-print("Welcome to the multiplication trainer %s!" % trainer.version())
+    # TODO Window name according to language
+    # TODO Implement multilingual support.
+    print("Hello!", user_stats[0]['name'])
+    print("Welcome to the multiplication trainer %s!" % trainer.version())
 
-while True:
-    print("\nMain Menu Options:\n1 - Practice Multiplications!\n2 - See Statistics\n3 - Settings\n4 - About\n5 - Exit")
-    selection = int(input("\nWhat do you want to do?\n"))
+    while True:
+        print(
+            "\nMain Menu Options:\n1 - Practice Multiplications!\n2 - See Statistics\n3 - Settings\n4 - About\n5 - Exit")
+        selection = int(input("\nWhat do you want to do?\n"))
 
-    if selection == 1:
-        q = int(input("\nHow many exercises do you want to do?\n"))
+        if selection == 1:
+            q = int(input("\nHow many exercises do you want to do?\n"))
 
-        for x in range(q):
-            exercise.get_exercise()
-            print("%s * %s = ?" % (str(exercise.current_exercise[0] + 1), str(exercise.current_exercise[1] + 1)))
-            answer = int(input())
+            for x in range(q):
+                exercise.get_exercise()
+                print("%s * %s = ?" % (str(exercise.current_exercise[0] + 1), str(exercise.current_exercise[1] + 1)))
+                answer = int(input())
 
-            if exercise.check_answer(answer) is True:
-                print("That's right!\n")
-                winsound.PlaySound("ding.wav", winsound.SND_ASYNC)
-            else:
-                # If this is another elif to check if is False then the historic and other counters will increase twice.
-                print("Wrong! The answer was ", str(exercise.answer), "\n")
-                winsound.PlaySound("buzz.wav", winsound.SND_ASYNC)
+                if exercise.check_answer(answer) is True:
+                    print("That's right!\n")
+                    winsound.PlaySound("ding.wav", winsound.SND_ASYNC)
+                else:
+                    # If this is another elif to check if is False then the historic and other counters will increase twice.
+                    print("Wrong! The answer was ", str(exercise.answer), "\n")
+                    winsound.PlaySound("buzz.wav", winsound.SND_ASYNC)
 
-    elif selection == 2:
-        print("\nStatistics by Multiplication table:")
-        pprint.pprint(exercise.confidence.data)
+        elif selection == 2:
+            print("\nStatistics by Multiplication table:")
+            pprint.pprint(exercise.confidence.data)
 
-    elif selection == 3:
-        print("\nSettings:\n1 - Reset statistics\n2 - Edit your name\n3 - Go back to the main menu")
-        sub_selection = int(input("\nWhat do you want to do?\n"))
-        if sub_selection == 1:
-            trainer.reset_user_stats()
-            print("Reset successful! All stats have been set to 0.")
-        elif sub_selection == 2:
-            trainer.edit_name()
-        elif sub_selection == 3:
-            continue
+        elif selection == 3:
+            print("\nSettings:\n1 - Reset statistics\n2 - Edit your name\n3 - Go back to the main menu")
+            sub_selection = int(input("\nWhat do you want to do?\n"))
+            if sub_selection == 1:
+                trainer.reset_user_stats()
+                print("Reset successful! All stats have been set to 0.")
+            elif sub_selection == 2:
+                trainer.edit_name()
+            elif sub_selection == 3:
+                continue
 
-    elif selection == 4:
-        print("Multiplication Trainer v.%s\nMade in Python 3\nAuthor: Ezra AA Cordova\nWebsite: www.ezrillex.club" %
-              trainer.version())
+        elif selection == 4:
+            print("Multiplication Trainer v.%s\nMade in Python 3\nAuthor: Ezra AA Cordova\nWebsite: www.ezrillex.club" %
+                  trainer.version())
 
-    elif selection == 5:
-        break
+        elif selection == 5:
+            break
---
+++ Launcher.py
@@ -0,0 +1,7 @@
+import CLI
+import trainer
+
+# TODO check file for interface mode
+
+# Launch the CLI
+CLI.cli()
